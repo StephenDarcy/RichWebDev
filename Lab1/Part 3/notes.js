f1b74e68262f0c0b6718ce084d16e0ce7e0f450a
Stephen Darcy <stephendarcy123@gmail.com>
2021-10-01 00:45:59 +0100

    finished

--- Lab1/Part 3/notes.js
+++ Lab1/Part 3/notes.js
@@ -1,4 +1,5 @@
 var colourHighlight = "colour-click";
+var noteID = 1;
 
 $(document).ready(function() {
     $(".colour").click(function() {
@@ -11,6 +12,28 @@ $(document).ready(function() {
     $(".save-button").click(function(e) {
         e.preventDefault();
         var noteText = $("#textarea").val();
-        $(".notes-list").append("<li>" + noteText + "</li>");
+        $(".notes-list").append(
+            "<li id='list" +
+            noteID +
+            "'><div class='note' id='note" +
+            noteID +
+            "'><button class='delete-button' id='" +
+            noteID +
+            "'> X </button>" +
+            "<textarea class = 'textOnNote' id='text" +
+            noteID +
+            "'>" +
+            noteText +
+            "</textarea></div></li>"
+        );
+        var noteColour = $("#textarea").css("background-color");
+        $("#text" + noteID).css("background", noteColour);
+        $("#note" + noteID).css("background", noteColour);
+        noteID++;
+        $("#textarea").val("");
+    });
+
+    $("ul").on("click", ".delete-button", function() {
+        $(this).parent().remove();
     });
 });
