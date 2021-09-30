4237214c0bb8beb3b7fea4d02114a7d2a41825c0
Stephen Darcy <stephendarcy123@gmail.com>
2021-09-30 15:47:49 +0100

    notes text now displays on right panel

--- Lab1/Part 3/notes.js
+++ Lab1/Part 3/notes.js
@@ -1,10 +1,16 @@
 var colourHighlight = "colour-click";
 
-$(document).ready(function () {
-  $(".colour").click(function () {
-    var clicked = $(this).css("background-color");
-    $("#textarea").css("background-color", clicked);
-    $(".colour").removeClass(colourHighlight);
-    $(this).addClass(colourHighlight);
-  });
+$(document).ready(function() {
+    $(".colour").click(function() {
+        var clicked = $(this).css("background-color");
+        $("#textarea").css("background-color", clicked);
+        $(".colour").removeClass(colourHighlight);
+        $(this).addClass(colourHighlight);
+    });
+
+    $(".save-button").click(function(e) {
+        e.preventDefault();
+        var noteText = $("#textarea").val();
+        $(".notes-list").append("<li>" + noteText + "</li>");
+    });
 });
