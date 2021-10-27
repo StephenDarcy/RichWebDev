9fd082feb3591bd4e21173a011733af8eba55039
Stephen Darcy <stephendarcy123@gmail.com>
2021-10-27 23:39:46 +0100

    work on sorting the table

--- Lab2/Part 1/phone-directory.js
+++ Lab2/Part 1/phone-directory.js
@@ -7,7 +7,59 @@ window.onload = function() {
         e.preventDefault();
         addToContacts();
     });
+
+    document.querySelectorAll(".table-head").forEach((item) => {
+        item.addEventListener("click", (event) => {
+            event.preventDefault();
+            sortTable(event.target.id);
+        });
+    });
 };
+
+function sortTable(id) {
+    let table = document.getElementById("contacts");
+    let currentlySorting = true;
+    let dataColumn;
+
+    //setting which <td> to query
+    if (id == "name") {
+        dataColumn = 0;
+    } else if (id == "phone") {
+        dataColumn = 1;
+    } else {
+        dataColumn = 2;
+    }
+
+    //loop to continue sorting while this is true
+    while (currentlySorting) {
+        //setting to false to break loop once sorting is complete
+        currentlySorting = false;
+        let rows = table.rows;
+        let sortRows = false;
+        var i;
+
+        //looping through every row comparing the current with the next
+        for (i = 1; i < rows.length - 1; i++) {
+            console.log(i);
+            let currentRow = rows[i].getElementsByTagName("TD")[dataColumn];
+            let nextRow = rows[i + 1].getElementsByTagName("TD")[dataColumn];
+
+            console.log(currentRow, nextRow);
+            //checking to see if current row should be above next row
+            if (currentRow.innerHTML.toLowerCase() > nextRow.innerHTML.toLowerCase) {
+                //if current row needs to be sorted
+                sortRows = true;
+                break;
+            }
+        }
+
+        //switching the rows if they need to be sorted and setting the loop to continue sorting
+        if (sortRows) {
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            currentlySorting = true;
+        }
+    }
+}
 
 function addToContacts() {
     //clear error text
