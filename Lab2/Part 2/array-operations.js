f3cd97b005a8c76cdf67b58974fbb93a9cfc98aa
Stephen Darcy <stephendarcy123@gmail.com>
2021-11-04 21:45:10 +0000

    part 2 done bar line breaks

--- Lab2/Part 2/array-operations.js
+++ Lab2/Part 2/array-operations.js
@@ -5,23 +5,48 @@
  * HTML file included to make use of fetch API in browser
  */
 
+// Part 1
 fetch("https://jsonplaceholder.typicode.com/posts")
-    .then((response) => response.json())
-    .then((json) => postTitles(json));
+  .then((response) => response.json())
+  .then((json) => postTitles(json));
 
 /**
  * Function that prints all the post titles with more than 6 words
  * @param {string} json a JSON string containing an array of objects which contain the titles
  */
 function postTitles(json) {
-    const titles = json.map((a) => a.title);
-    const filteredTitles = titles.filter(wordCount);
+  const titles = json.map((a) => a.title);
+  const filteredTitles = titles.filter(wordCount);
 
-    function wordCount(title) {
-        if (title.split(/\s+/).length > 6) {
-            return title;
-        }
+  function wordCount(title) {
+    if (title.split(/\s+/).length > 6) {
+      return title;
     }
+  }
 
-    console.log(filteredTitles);
+  console.log(filteredTitles);
+}
+
+// Part 2
+fetch("https://jsonplaceholder.typicode.com/posts")
+  .then((response) => response.json())
+  .then((json) => wordMap(json));
+
+/**
+ * A function that extracts all the bodys from posts amd displays a word frequency map
+ * @param {string} json a JSON string containing an array of objects which contain the titles
+ */
+function wordMap(json) {
+  const bodies = json.map((a) => a.body);
+  console.log(bodies);
+
+  const wordsArray = bodies.flatMap((body) => body.split(" "));
+  const arrayMap = {};
+
+  console.log(wordsArray);
+  wordsArray.forEach((element) => {
+    let count = arrayMap[element] || 0;
+    arrayMap[element] = count + 1;
+  });
+  console.log(arrayMap);
 }
