dc6b961d28da6c7430fc3a9778705435de501151
Stephen Darcy <stephendarcy123@gmail.com>
2021-11-25 02:02:56 +0000

    Lab 3 part 1 started

---
+++ Lab3/Part 1/app.js
@@ -0,0 +1,16 @@
+var express = require("express");
+//use the application off of express.
+var app = express();
+
+app.use(express.static(__dirname + "/views"));
+
+//define the route for "/"
+app.get("/", function (request, response) {
+  //show this file when the "/" is requested
+  response.sendFile(__dirname + "/views/notes.html");
+});
+
+//start the server
+app.listen(8080);
+
+console.log("Running at http://localhost:8080");
