f8090212506edccc638dc43fc5e114714d915128
Stephen Darcy <stephendarcy123@gmail.com>
2021-11-28 14:21:08 +0000

    finished part 2

--- Lab3/Part 2/views/bundle.js
+++ Lab3/Part 2/views/bundle.js
@@ -7799,6 +7799,43 @@ function createInvalidObservableTypeError(input) {
 exports.createInvalidObservableTypeError = createInvalidObservableTypeError;
 
 },{}],225:[function(require,module,exports){
-const { fromEvent } = require("rxjs");
+const { fromEvent, interval, timer, take } = require("rxjs");
+
+let startButton = document.getElementById("start-button");
+let timerDisplay = document.getElementById("timer");
+let countdownActive = false;
+
+let timerClick = fromEvent(startButton, "click").subscribe(() => {
+  let hours = document.getElementById("hours").value * 3600 || 0;
+  let minutes = document.getElementById("minutes").value * 60 || 0;
+  let seconds = document.getElementById("seconds").value || 0;
+
+  //get total number of seconds
+  let secondsTotal = parseInt(seconds) + parseInt(minutes) + parseInt(hours);
+  countdownActive = !countdownActive;
+  if (!countdownActive) {
+    timerClick.unsubscribe();
+  } else {
+    countdown(secondsTotal);
+  }
+});
+
+function countdown(seconds) {
+  console.log(seconds);
+  const interval$ = interval(1000);
+  const timer$ = timer(seconds * 100);
+  const count$ = interval$.pipe(take(seconds));
+  count$.subscribe((time) => {
+    timerDisplay.innerHTML = convertToHMS(seconds - (time + 1));
+  });
+}
+
+function convertToHMS(seconds) {
+  var hours = Math.floor(seconds / (60 * 60));
+  seconds -= hours * (60 * 60);
+  var minutes = Math.floor(seconds / 60);
+  seconds -= minutes * 60;
+  return (0 < hours ? hours + ":" : "") + minutes + ":" + seconds;
+}
 
 },{"rxjs":2}]},{},[225]);
--- Lab3/Part 2/views/countdown.js
+++ Lab3/Part 2/views/countdown.js
@@ -1,1 +1,32 @@
-const { fromEvent } = require("rxjs");
+const { fromEvent, interval, timer, take } = require("rxjs");
+
+let startButton = document.getElementById("start-button");
+let timerDisplay = document.getElementById("timer");
+
+fromEvent(startButton, "click").subscribe(() => {
+  let hours = document.getElementById("hours").value * 3600 || 0;
+  let minutes = document.getElementById("minutes").value * 60 || 0;
+  let seconds = document.getElementById("seconds").value || 0;
+
+  //get total number of seconds
+  let secondsTotal = parseInt(seconds) + parseInt(minutes) + parseInt(hours);
+  countdown(secondsTotal);
+});
+
+function countdown(seconds) {
+  console.log(seconds);
+  const interval$ = interval(1000);
+  const timer$ = timer(seconds * 100);
+  const count$ = interval$.pipe(take(seconds));
+  count$.subscribe((time) => {
+    timerDisplay.innerHTML = convertToHMS(seconds - (time + 1));
+  });
+}
+
+function convertToHMS(seconds) {
+  var hours = Math.floor(seconds / (60 * 60));
+  seconds -= hours * (60 * 60);
+  var minutes = Math.floor(seconds / 60);
+  seconds -= minutes * 60;
+  return (0 < hours ? hours + ":" : "") + minutes + ":" + seconds;
+}
